4f5b8a58c0329375e360d1d146c1a1fcfd2e8a8c
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-19 20:49:45 +0530

    feat: report repository

--- BookShoppingCartMvcUI/Program.cs
+++ BookShoppingCartMvcUI/Program.cs
@@ -12,18 +12,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services
-    .AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultUI()
     .AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<IHomeRepository,HomeRepository>();
-builder.Services.AddTransient<ICartRepository,CartRepository>();
-builder.Services.AddTransient<IUserOrderRepository,UserOrderRepository>();
+builder.Services.AddTransient<IHomeRepository, HomeRepository>();
+builder.Services.AddTransient<ICartRepository, CartRepository>();
+builder.Services.AddTransient<IUserOrderRepository, UserOrderRepository>();
 builder.Services.AddTransient<IStockRepository, StockRepository>();
 builder.Services.AddTransient<IGenreRepository, GenreRepository>();
 builder.Services.AddTransient<IFileService, FileService>();
 builder.Services.AddTransient<IBookRepository, BookRepository>();
+builder.Services.AddTransient<IReportRepository, ReportRepository>();
+
 var app = builder.Build();
 // Uncomment it when you run the project first time, It will registered an admin
 //using (var scope = app.Services.CreateScope())
