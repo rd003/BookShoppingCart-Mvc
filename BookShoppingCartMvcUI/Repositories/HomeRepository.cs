68c2f0885967fe9dc30671988981655c9f013b8f
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-03-06 11:40:30 +0530

    home repo get books refactored

--- BookShoppingCartMvcUI/Repositories/HomeRepository.cs
+++ BookShoppingCartMvcUI/Repositories/HomeRepository.cs
@@ -19,32 +19,34 @@ namespace BookShoppingCartMvcUI.Repositories
         }
         public async Task<IEnumerable<Book>> GetBooks(string sTerm = "", int genreId = 0)
         {
-            sTerm = sTerm.ToLower();
-            IEnumerable<Book> books = await (from book in _db.Books
-                         join genre in _db.Genres
-                         on book.GenreId equals genre.Id
-                         join stock in _db.Stocks
-                         on book.Id equals stock.BookId
-                         into book_stocks
-                         from bookWithStock in book_stocks.DefaultIfEmpty()
-                         where string.IsNullOrWhiteSpace(sTerm) || (book != null && book.BookName.ToLower().StartsWith(sTerm))
-                         select new Book
-                         {
-                             Id = book.Id,
-                             Image = book.Image,
-                             AuthorName = book.AuthorName,
-                             BookName = book.BookName,
-                             GenreId = book.GenreId,
-                             Price = book.Price,
-                             GenreName = genre.GenreName,
-                             Quantity=bookWithStock==null? 0:bookWithStock.Quantity
-                         }
-                         ).ToListAsync();
+
+            var bookQuery = _db.Books
+                           .Include(x => x.Genre)
+                           .Include(x => x.Stock)
+                           .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(sTerm))
+            {
+                bookQuery = bookQuery.Where(b => b.BookName.StartsWith(sTerm.ToLower()));
+            }
+
             if (genreId > 0)
             {
-
-                books = books.Where(a => a.GenreId == genreId).ToList();
+                bookQuery = bookQuery.Where(b => b.GenreId == genreId);
             }
+
+            var books = await bookQuery.Select(book => new Book
+            {
+                Id = book.Id,
+                Image = book.Image,
+                AuthorName = book.AuthorName,
+                BookName = book.BookName,
+                GenreId = book.GenreId,
+                Price = book.Price,
+                GenreName = book.Genre.GenreName,
+                Quantity = book.Stock == null ? 0 : book.Stock.Quantity
+            }).ToListAsync();
+
             return books;
 
         }
